95b8c2057dea69a0b3b036b6f1391e25e7694364
Felix <zen9.felix@gmail.com>
2021-04-24 14:07:23 +0800

    Pin version with niv

--- release.nix
+++ release.nix
@@ -1,11 +1,16 @@
-{ compiler ? "ghc8103", pkgs ? import <nixpkgs> { } }:
+{ compiler ? "ghc8104", sources ? import ./nix/sources.nix }:
+
+with { overlay = _: pkgs: { niv = (import sources.niv { }).niv; }; };
 
 let
+  pkgs = import sources.nixpkgs {
+    overlays = [ overlay ];
+    config = { };
+  };
   haskellPackages = pkgs.haskell.packages.${compiler};
   project = haskellPackages.callPackage ./cabal.nix { };
 in {
   project = project;
-
   shell = haskellPackages.shellFor {
     packages = p: with p; [ project ];
     buildInputs = with haskellPackages; [
